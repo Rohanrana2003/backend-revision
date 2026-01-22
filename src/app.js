a992fb7e37d5b2fdd296edb160525feb497d875f
Rohan rana <rohanrana33042@gmail.com>
2026-01-23 00:35:41 +0530

    Created CRUD APIs

--- src/app.js
+++ src/app.js
@@ -3,17 +3,71 @@ const connectToDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
+// Signup API
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firtName: "Rohan",
-    lastName: "Rana",
-    emailId: "Rohan@gmail.com",
-    password: "rohanrohan",
-  });
+  const user = new User(req.body);
 
-  await user.save();
+  try {
+    await user.save();
+    res.send("User Created Successfully!");
+  } catch (err) {
+    res.status(400).send("Error in Saving the user:" + err.message);
+  }
+});
 
-  res.send("User Created Successfully!");
+// GET users by EmailId API
+app.get("/userbyId", async (req, res) => {
+  const EmailId = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: EmailId });
+    if (users.length === 0) {
+      res.status(404).send("Users not found");
+    } else {
+      res.send(users);
+    }
+
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("Somethng went wrong");
+  }
+});
+
+// GET all users API
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("Users not found");
+  }
+});
+
+// Update User by Id
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User Updated Successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Delete User by Id
+app.delete("/user", async (req, res) => {
+  const EmailId = req.body.emailId;
+
+  try {
+    await User.findOneAndDelete({ emailId: EmailId });
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
 });
 
 connectToDB()
